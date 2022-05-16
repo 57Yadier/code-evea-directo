63b175d773bd271e0d0d27184cf5e3ca6bb0caa8
57Yadier <103086948+57Yadier@users.noreply.github.com>
2022-05-16 14:49:03 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
-api_hash = "apihash"
-api_id = 1736372819
-bot_token = "bottoken"
-authorized_users = []
-admin_user = 'admindelbot'
+api_hash = "80ae053901c049b519ea5bf0f50e1675"
+api_id = 17345593
+bot_token = "5134825599:AAEY-hV7r4lFnqIbtAX9ihPtwOS3P-R_jgQ"
+authorized_users = [countryandlifee]
+admin_user = 'STANISLASKI'
